52ce84ddf1856766389344cc18fa4dd300bd09f4
Erica Hagle <ericahagle.dev@gmail.com>
2023-07-08 10:56:10 -0700

    Add string variable reassignments

--- variables.js
+++ variables.js
@@ -3,13 +3,25 @@ var stringOne = "string 1";
 var stringTwo = "string 2";
 var stringThree = "string 3";
 
+console.log(stringOne + ", " + stringTwo + ", " + stringThree);
+
 // Numbers
 var numberOne = 1;
 var numberTwo = 2;
 var numberThree = 3;
+
+console.log(numberOne + ", " + numberTwo + ", " + numberThree);
 
 // Booleans
 var booleanOne = true;
 var booleanTwo = false;
 var booleanThree = true;
 
+console.log(booleanOne + ", " + booleanTwo + ", " + booleanThree);
+
+// Reassigned string variables
+stringOne = "string one";
+stringTwo = "string two";
+stringThree = "string three";
+
+console.log(stringOne + ", " + stringTwo + ", " + stringThree);
